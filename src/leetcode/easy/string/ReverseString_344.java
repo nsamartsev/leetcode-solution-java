ffd5203559d13caace8c1a1960642980413683af
nsmrtzv <samartsevnikita@gmail.com>
2023-07-25 12:23:51 +0200

    344 - use two pointers

--- src/leetcode/easy/string/ReverseString_344.java
+++ src/leetcode/easy/string/ReverseString_344.java
@@ -1,19 +1,27 @@
 package leetcode.easy.string;
 
+import java.util.Arrays;
+
 public class ReverseString_344 {
     public static void main(String[] args) {
-//        char[] input = {'h', 'e', 'l', 'l', 'o'};
-        char[] input = {'t','h','i','s','i','s','t','e','s','t' };
+        char[] input = {'h', 'e', 'l', 'l', 'o'};
         reverseString(input);
+
+        assert Arrays.equals(input, new char[]{'o', 'l', 'l', 'e', 'h'});
     }
 
     public static void reverseString(char[] s) {
         int lenght = s.length;
 
-        for (int i = 0, j = lenght-1; i < lenght/2; i++, j--) {
-            char tmp = s[i];
-            s[i] = s[j];
-            s[j] = tmp;
+        int l = 0;
+        int r = lenght - 1;
+
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
         }
     }
 }
